d86bec5c3af099a66f0948272a87812fe6bb387d
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-10-07 17:15:15 +0700

    FE TOAN RAU

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment.prod';
+import {SignUpForm} from '../model/SignUpForm';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+//API_LOCAL
+//   private API_SIGNUP = environment.API_LOCAL+'signup';
+  //API_SERVER
+  private API_SIGNUP =environment.API_SERVER+'signup';
+  constructor(private http: HttpClient) { }
+  signUp(signUp: SignUpForm): Observable<any>{
+    return this.http.post<any>(this.API_SIGNUP, signUp)
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  API_LOCAL: 'http://localhost:8080/',
+  API_SERVER: 'https://c04-toan-rau.herokuapp.com/',
+  production: true
+};
